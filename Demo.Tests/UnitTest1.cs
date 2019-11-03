c256d681194cfd3ab714b480ca6d2f08cd8e8357
Martin Lind Gregersen <mlg@hubahuba.dk>
2019-11-03 18:01:38 +0100

    Tests added

---
+++ Demo.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Demo.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
